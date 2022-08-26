935d8f65ce067317c0814f4fb24e2f0017985510
brenodanyel <bdls1704@hotmail.com>
2022-08-26 15:00:16 -0300

    add custom error handling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import { ErrorMiddleware } from './helpers/error';
 
 export class App {
   app: express.Express;
 
   constructor() {
     this.app = express();
+    this.app.use(express.json());
     this.app.get('/', (_, res) => res.send('Hello World'));
   }
 
@@ -13,6 +15,6 @@ export class App {
   }
 
   public addRoute(path: string, route: express.Router) {
-    this.app.use(path, route);
+    this.app.use(path, route, ErrorMiddleware);
   };
 }
---
+++ src/helpers/error.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class CustomError extends Error {
+  public code: number;
+
+  constructor(code: number, message: string) {
+    super(message);
+    this.code = code;
+  }
+}
+
+export const ErrorMiddleware = (error: CustomError, _req: Request, res: Response, _next: NextFunction) => {
+  if (error instanceof CustomError) {
+    res.status(error.code).json({ error: error.message });
+    return;
+  }
+
+  console.log(error);
+
+  res.status(500).json({ error: 'Erro interno' });
+};
