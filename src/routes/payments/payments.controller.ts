6ffe3fa5b1531222de660ae6d32aa3eb9fdfd264
brenodanyel <bdls1704@hotmail.com>
2022-08-31 18:29:07 -0300

    refactor payments.controller

--- src/routes/payments/payments.controller.ts
+++ src/routes/payments/payments.controller.ts
@@ -1,6 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
-import { Service } from '../service/payments.service';
-import { Token } from '../helpers/token';
+import { RequestHandler } from 'express';
+import { Service } from './payments.service';
+import { Token } from '../../helpers/token';
 
 export class Controller {
   service: Service;
@@ -9,14 +9,11 @@ export class Controller {
   constructor(service = new Service(), token = new Token()) {
     this.service = service;
     this.token = token;
-
-    this.findByUser = this.findByUser.bind(this);
-    this.createByUser = this.createByUser.bind(this);
-    this.deleteByUser = this.deleteByUser.bind(this);
-    this.updateByUser = this.updateByUser.bind(this);
   };
 
-  public async findByUser(req: Request, res: Response, next: NextFunction) {
+  public findByUser: RequestHandler<
+    {}, {}, {}
+  > = async (req, res, next) => {
     try {
       const token = Token.extract(String(req.headers.authorization));
       const { user } = await this.token.verify(token);
@@ -25,28 +22,25 @@ export class Controller {
     } catch (e) {
       next(e);
     }
-  }
+  };
 
-  public async createByUser(
-    req: Request<{}, {}, { customer: string; installments: number; price: number; }>,
-    res: Response,
-    next: NextFunction
-  ) {
+  public createByUser: RequestHandler<
+    {}, {}, { customer: string; installments: number; price: number; }
+  > = async (req, res, next) => {
     try {
       const token = Token.extract(String(req.headers.authorization));
       const { user } = await this.token.verify(token);
-      const result = await this.service.createByUser({ ...req.body, userId: user.id });
+      const { customer, installments, price } = req.body;
+      const result = await this.service.createByUser({ customer, installments, price, userId: user.id });
       res.status(201).json(result);
     } catch (e) {
       next(e);
     }
-  }
+  };
 
-  public async deleteByUser(
-    req: Request<{ id: string; }, {}, {}>,
-    res: Response,
-    next: NextFunction,
-  ) {
+  public deleteByUser: RequestHandler<
+    { id: string; }, {}, {}
+  > = async (req, res, next) => {
     try {
       const token = Token.extract(String(req.headers.authorization));
       const { user } = await this.token.verify(token);
@@ -55,20 +49,19 @@ export class Controller {
     } catch (e) {
       next(e);
     }
-  }
+  };
 
-  public async updateByUser(
-    req: Request<{ id: string; }, {}, { customer: string; installments: number; price: number; }>,
-    res: Response,
-    next: NextFunction
-  ) {
+  public updateByUser: RequestHandler<
+    { id: string; }, {}, { customer: string; installments: number; price: number; }
+  > = async (req, res, next) => {
     try {
       const token = Token.extract(String(req.headers.authorization));
       const { user } = await this.token.verify(token);
-      const result = await this.service.updateByUser(user.id, req.params.id, req.body);
+      const { customer, installments, price } = req.body;
+      const result = await this.service.updateByUser(user.id, req.params.id, { customer, installments, price });
       res.status(200).json(result);
     } catch (e) {
       next(e);
     }
-  }
+  };
 }
