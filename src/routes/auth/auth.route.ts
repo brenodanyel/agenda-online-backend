7c1db48f45ccf4b15048dd5cb8ee93714384d5a3
brenodanyel <bdls1704@hotmail.com>
2022-08-31 18:27:50 -0300

    refactor auth.route

---
+++ src/routes/auth/auth.route.ts
@@ -0,0 +1,18 @@
+import { Router as ExpressRouter } from 'express';
+import { Controller } from './auth.controller';
+import { Middleware } from './auth.middleware';
+
+export class Router {
+  public router = ExpressRouter();
+
+  constructor(controller = new Controller()) {
+    this.router.route('/login')
+      .post(Middleware.login, controller.login);
+
+    this.router.route('/register')
+      .post(Middleware.register, controller.register);
+
+    this.router.route('/verify')
+      .get(Middleware.verify, controller.verify);
+  }
+}
